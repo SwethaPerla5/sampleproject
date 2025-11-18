369ea874305fbbee935da8cbaeaa35ed3f888062
SwethaPerla5 <93303401+SwethaPerla5@users.noreply.github.com>
2025-11-18 00:20:27 -0800

    Update test.py

--- strings/test.py
+++ strings/test.py
@@ -1,1 +1,8 @@
-
+s = "python"
+# print the first char and last char
+print(s[0],s[1])
+# Given a string s, print the string in reverse without using reversed() or functions.
+print(s[::-1])
+# Given an odd-length string, extract the middle 3 characters using slicing.
+p =  "abcdefg"
+print(p[3:6])
